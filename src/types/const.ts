c6037de79717d9ba979d87836766eb1edaa93763
mamadgm <84960554+mamadgm@users.noreply.github.com>
2025-06-17 17:04:20 +0330

    Updates

--- src/types/const.ts
+++
@@ -1,6 +0,0 @@
-export const API_BASE_URL = 'https://apieventech.mammutepd.ir'
-export const TOAST_MESSAGES = {
-  LOGIN_SUCCESS: 'با موفقیت وارد شدید',
-  LOGIN_FAILURE: 'مشکل در ورود',
-  INVALID_CREDENTIALS: 'نام کاربری یا رمز عبور نادرست است',
-}
